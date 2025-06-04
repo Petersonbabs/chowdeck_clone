3d60d2cbfb868826e012ef179f1905af372024c8
zoologistboi <aminuoluwaseun3@gmail.com>
2025-06-04 16:02:38 -0400

    utils added and jwt token function exported, user can now use

---
+++ utils/jwt.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+const dotenv = require("dotenv")
+dotenv.config() 
+
+const secretKey = process.env.jwt_secret
+
+const expireIn = process.env.expireToken
+ 
+
+const generateToken = (user)=>{
+     try {
+        const accessToken =  jwt.sign(
+            {id:user._id, email:user.email},
+             secretKey,
+            {expiresIn:expireIn})
+
+            return accessToken
+
+        
+     } catch (error) {
+       console.error("JWT generation error:", error);
+        throw error; 
+     }
+
+    }
+    module.exports = generateToken
