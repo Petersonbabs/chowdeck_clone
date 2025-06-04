b58f8fddbc780948e7a7e4950ce9c65bda02618a
Abdullah12-tech <Abdullahakinkunmi26@gmail.com>
2025-06-04 22:53:19 +0100

    changes made

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,16 +25,4 @@ const signup = (req,res,next)=>{
 }
 module.exports = {
     signup
-}const bcrypt = require("bcryptjs");
-
-const signup = (req,res,next)=>{
-    const {password} = req.body;
-    const salt = bcrypt.genSalt(10);
-    const hashedPassword = bcrypt.hash(password, salt);
-    try {
-        const user = user
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
 }
--- models/user.js
+++ models/user.js
@@ -5,16 +5,7 @@ console.log("coach")
 
 
 console("seunnnnn")
-<<<<<<< HEAD
-console.log("Samuel")
-
-console.log("Abdullah is here");
-
-
-
-=======
 
 console.log("Abdullah is here");
 
 console.log("Samuel")
->>>>>>> upstream/main
