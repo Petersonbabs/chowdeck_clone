1c8c9aedbd26cb1f53153c08ddbea01c7eba50c0
Psalmstrings <psalmstrings@gmail.com>
2025-06-05 01:06:25 -0700

    authRouter Updated

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const authRouter = express.Router()
-const {signup, login, verifyEmail} = require("../controllers/authController")
+const { signup } = require("../controllers/authController")
+const logoutController = require("../controllers/logout")
+const { signIn } = require("../controllers/auth")
 
 
+
+authRouter.post("/signup", signup)
+authRouter.post("/signin", signIn)
+authRouter.post("/logout", logoutController)
 module.exports = authRouter
